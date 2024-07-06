d32d9989529dc8eb67317a947cf7cebcabc409e6
SergioIgnatev <ignatevsergey17@gmail.com>
2024-07-06 21:37:01 +0300

    adding tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+<h1 class="text-red-900">Justinian the great</h1>
+Hello vue
+<h1 class="text-[400px]">TEST</h1>
   </div>
 </template>

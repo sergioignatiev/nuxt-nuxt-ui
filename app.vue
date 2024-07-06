8215bd337f79c2750a49a944060da404d907359a
SergioIgnatev <ignatevsergey17@gmail.com>
2024-07-06 22:20:26 +0300

    navigation

--- app.vue
+++ app.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
+    <TheNav/>
+    <UCheckbox  name="notifications" label="Notifications" />
+    <UAuthForm
+     title="Login"
+    icon="i-heroicons-user-circle"
+    ></UAuthForm>
 <h1 class="text-red-900">Justinian the great</h1>
 <UButton label="Button" />
 <h1 class="text-[400px]">TEST</h1>
   </div>
+  <TheFooter/>
 </template>

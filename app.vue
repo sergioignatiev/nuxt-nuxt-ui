1cb0f9152d250ca0df9b0bd5323ad00667c08440
SergioIgnatev <ignatevsergey17@gmail.com>
2024-07-07 18:29:00 +0300

    test

--- app.vue
+++ app.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="flex justify-center items-center"> 
-    <img :src="image" class="w-[1000px]" alt="альтернативный текст"> 
-</div>
-  <div>
-      <UInput class="m-3" color="black" variant="outline" placeholder="Search..."/>
-    <TheNav/>
-    <UCheckbox  name="notifications" label="Notifications" />
-    <UAuthForm
-     title="Login"
-    icon="i-heroicons-user-circle"
-    ></UAuthForm>
-<h1 class="text-red-900 text-[40px]">Justinian the great</h1>
-<UButton label="Button" />
+<div class="bg-slate-100 min-h-[100vh]">
 
-  </div>
-  <TheFooter/>
+<div class="flex flex-wrap">
+  <TheCard label="Сочный"
+ context="Наш тариф лорем ипсуи"
+ image="https://upload.wikimedia.org/wikipedia/commons/a/a0/Catalinamicaelaspain65.jpg"
+ price="999"
+ />
+ <TheCard label="Сочный"
+ context="Наш тариф лорем ипсуи"
+ image="https://upload.wikimedia.org/wikipedia/commons/a/a0/Catalinamicaelaspain65.jpg"
+ price="999"
+ />
+</div>
+</div>
 </template>
 <script setup>
-const image=ref('https://cdn.prod.website-files.com/5e0f1144930a8bc8aace526c/65dae4c7b0efe4cb00893d99_65da80b1299e167d189d9cd0-895091ec3d7f4332651f67fcfa5db74c.jpeg')
+
 </script>
---
+++ components/TheCard.vue
@@ -0,0 +1,18 @@
+<template>
+  
+  <div class="w-[300px] p-4 min-h-[300px] bg-white border-[3px]  rounded-lg">
+    <h1 class="font-extrabold text-[30px]">{{ label }}</h1>
+    <img :src="image" :alt="label">
+    <p class="font-bold">{{ context }}</p>
+  <p class="font-bold">{{ price }} p</p>
+  <UButton>Выбрать</UButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps(['label','context','image','price'])
+</script>
+
+<style>
+
+</style>

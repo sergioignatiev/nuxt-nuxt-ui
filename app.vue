7c2c2cf3f905f0caa426fac4fb23e97b3c8767b8
SergioIgnatev <ignatevsergey17@gmail.com>
2024-07-07 15:08:33 +0300

    db

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="flex justify-center items-center"> 
+    <img :src="image" class="w-[1000px]" alt="альтернативный текст"> 
+</div>
   <div>
       <UInput class="m-3" color="black" variant="outline" placeholder="Search..."/>
     <TheNav/>
@@ -13,3 +16,6 @@
   </div>
   <TheFooter/>
 </template>
+<script setup>
+const image=ref('https://cdn.prod.website-files.com/5e0f1144930a8bc8aace526c/65dae4c7b0efe4cb00893d99_65da80b1299e167d189d9cd0-895091ec3d7f4332651f67fcfa5db74c.jpeg')
+</script>
--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,29 +1,41 @@
-<script setup lang="ts">
-const route = useRoute()
 
-const links = [{
-  label: 'Profile',
-  avatar: {
-    src: 'https://avatars.githubusercontent.com/u/739984?v=4'
-  },
-  badge: 100
-}, {
-  label: 'Installation',
-  icon: 'i-heroicons-home',
-  to: '/getting-started/installation'
-}, {
-  label: 'Vertical Navigation',
-  icon: 'i-heroicons-chart-bar',
-  to: `${route.path.startsWith('/dev') ? '/dev' : ''}/components/vertical-navigation`
-}, {
-  label: 'Command Palette',
-  icon: 'i-heroicons-command-line',
-  to: '/components/command-palette'
-}]
+<script setup lang="ts">
+const items = [
+  [{
+    label: 'Profile',
+    avatar: {
+      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+    }
+  }], [{
+    label: 'Edit',
+    icon: 'i-heroicons-pencil-square-20-solid',
+    shortcuts: ['E'],
+    click: () => {
+      console.log('Edit')
+    }
+  }, {
+    label: 'Duplicate',
+    icon: 'i-heroicons-document-duplicate-20-solid',
+    shortcuts: ['D'],
+    disabled: true
+  }], [{
+    label: 'Archive',
+    icon: 'i-heroicons-archive-box-20-solid'
+  }, {
+    label: 'Move',
+    icon: 'i-heroicons-arrow-right-circle-20-solid'
+  }], [{
+    label: 'Delete',
+    icon: 'i-heroicons-trash-20-solid',
+    shortcuts: ['⌘', 'D']
+  }]
+]
 </script>
 
 <template>
-  <UVerticalNavigation :links="links" />
+  <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+    <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
+  </UDropdown>
 </template>
 
 
